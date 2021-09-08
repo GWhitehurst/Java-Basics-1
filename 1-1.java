8a7f04664d7753209fecd698fb1453195fd08709
GWhitehurst <90222043+GWhitehurst@users.noreply.github.com>
2021-09-08 02:31:33 -0700

    First upload

---
+++ 1-1.java
@@ -0,0 +1,12 @@
+package com.gw.sse;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.print("1)\n*\n**\n***\n****\n.........\n");
+        System.out.print("2)\n.........\n****\n***\n**\n*\n");
+        System.out.print("3)\n     *\n    ***\n   *****\n  *******\n...........\n");
+        System.out.print("4)\n...........\n  *******\n   *****\n    ***\n     *\n");
+    }
+}
---
+++ 1-2.java
@@ -0,0 +1,30 @@
+package com.gw.sse;
+import java.lang.Math;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int num = (int)(Math.random() * 100 + 1);
+        Scanner uInput = new Scanner(System.in);
+        System.out.println(num);
+        int numGuesses = 0;
+
+        for(numGuesses = 5; numGuesses > 0; numGuesses--){
+            System.out.println("Guess a number between 1-100 (" + numGuesses + " guesses left)");
+            int uGuess = uInput.nextInt();
+            if(!(num - 10 <= uGuess && uGuess <= num + 10)) {
+                System.out.println("Not close enough");
+            }
+            else if(uGuess == num){
+                System.out.println("The correct number was " + num);
+                break;
+            }
+        }
+
+        if(numGuesses == 0){
+            System.out.println("Sorry, the correct number was " + num);
+        }
+
+    }
+}
