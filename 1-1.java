53f85d62349f7ae4d49475ef79231e1ffddbf04e
GWhitehurst <90222043+GWhitehurst@users.noreply.github.com>
2021-09-11 17:31:32 -0700

    Rewritten with loops

--- 1-1.java
+++ 1-1.java
@@ -3,10 +3,74 @@ package com.gw.sse;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        System.out.print("1)\n*\n**\n***\n****\n.........\n");
-        System.out.print("2)\n.........\n****\n***\n**\n*\n");
-        System.out.print("3)\n     *\n    ***\n   *****\n  *******\n...........\n");
-        System.out.print("4)\n...........\n  *******\n   *****\n    ***\n     *\n");
+        for(int i = 1; i <= 4; i++){
+            System.out.println(i + ")");
+            switch (i) {
+                case 1:
+                    for(int j = 1; j <= 5; j++){
+                        if(j == 5){
+                            printDot(9);
+                        }
+                        else {
+                            printStar(j);
+                        }
+                        System.out.print("\n");
+                    }
+                    break;
+                case 2:
+                    for(int j = 5; j >= 1; j--){
+                        if(j == 5){
+                            printDot(9);
+                        }
+                        else{
+                            printStar(j);
+                        }
+                        System.out.print("\n");
+                    }
+                    break;
+                case 3:
+                    for(int j = 1; j <= 5; j++){
+                        if(j == 5){
+                            printDot(11);
+                        }
+                        else{
+                            printSpace(6-j);
+                            printStar((j * 2) - 1);
+                        }
+                        System.out.print("\n");
+                    }
+                    break;
+                case 4:
+                    for(int j = 5; j >= 1; j--){
+                        if(j == 5){
+                            printDot(11);
+                        }
+                        else{
+                            printSpace(6-j);
+                            printStar((j * 2) - 1);
+                        }
+                        System.out.print("\n");
+                    }
+                    break;
+            }
+        }
+    }
+
+    public static void printDot(int count){
+        for(int i = 0; i < count; i++){
+            System.out.print(".");
+        }
+    }
+
+    public static void printStar(int count){
+        for(int i = 0; i < count; i++){
+            System.out.print("*");
+        }
+    }
+
+    public static void printSpace(int count){
+        for(int i = 0; i < count; i++){
+            System.out.print(" ");
+        }
     }
 }
